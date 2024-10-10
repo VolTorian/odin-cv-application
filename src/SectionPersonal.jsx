fd4069e758785878751086b8fc1e3b4738463bfe
VolTorian <33360939+VolTorian@users.noreply.github.com>
2024-10-10 15:45:44 -0700

    SectionPersonal component skeleton

---
+++ src/SectionPersonal.jsx
@@ -0,0 +1,22 @@
+function HandleInfo({info, description}) {
+    if (info) {
+        return <span>{description}: {info}&nbsp;&nbsp;&nbsp;</span>
+    }
+}
+
+function SectionPersonal({firstName, lastName, phone, email, linkedIn, location}) {
+    
+    return (
+        <div id="section-personal" className="section">
+            <h2>{firstName + " " + lastName}</h2>
+            <div>
+                <HandleInfo info={phone} description="Phone" />
+                <HandleInfo info={email} description="Email" />
+                <HandleInfo info={linkedIn} description="Location" />
+                <HandleInfo info={location} description="Location" />
+            </div>
+        </div>
+    )
+}
+
+export default SectionPersonal;
