c1b599e02cdd928b47eab42481dac8cedde86876
VolTorian <33360939+VolTorian@users.noreply.github.com>
2024-10-10 18:39:07 -0700

    split page into form side and resume side

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,10 @@ import SectionPersonal from './SectionPersonal'
 function App() {
     return (
         <>
-            <SectionPersonal firstName="John" lastName="Doe" phone="0123456789" email="testmail@testmail.com" location="somewhere"/>
+            <div id="forms">test</div>
+            <div id="resume">
+                <SectionPersonal firstName="John" lastName="Doe" phone="0123456789" email="testmail@testmail.com" location="somewhere"/>
+            </div>
         </>
     )
 }
